4a6724356564102b04b0d90b38dd57b71d7763e2
Daryl Busson <darylbusson@pop-os.localdomain>
2026-08-18 14:36:52 -0500

    Fixed accounts urls.py file

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,23 +1,19 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='api-login'),
     path('stats/', views.dashboard_stats, name='api-stats'),
-    
-    # NEW: Admin creates a user here
     path('create-user/', views.create_user_view, name='api-create-user'),
-
-    # -----------------------------------------------------
-    # PROFILE
-    # -----------------------------------------------------
-    path("profile/", ProfileView.as_view(), name="profile"),
-    path("update-profile/", UpdateProfileView.as_view()),
-
-    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
+    
+    # PROFILE (Updated with views. prefix)
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("update-profile/", views.UpdateProfileView.as_view()),
+    path("change-password/", views.ChangePasswordView.as_view(), name="change_password"),
     
     path('users/', views.list_users_view, name='list-users'),
     path('users/<int:user_id>/update/', views.update_user_view, name='update-user'),
-    
     path('notes/', views.manage_notes, name='manage-notes'),
 ]
+
