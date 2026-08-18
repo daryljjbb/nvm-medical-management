bc79dbc8aba57940837fb54b65f07885db4b4db7
Daryl Busson <darylbusson@pop-os.localdomain>
2026-08-18 15:12:38 -0500

    took text out of vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // 👈 Add this line to force absolute paths for all production assets
+  base: '/',
   plugins: [react()],
 })
 
