e6c75d127ecb5fdbf7831f4befee21d348443536
Daryl Busson <darylbusson@pop-os.localdomain>
2026-08-23 11:47:21 -0500

    fixed Layout.jsx import

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom"; // Essential for SPA performance
-import "./layou.css";
+import "./layout.css";
 import InactivityLogout from "./InactivityLogout.jsx";
 import { auth } from "../utils/auth.js";
 
