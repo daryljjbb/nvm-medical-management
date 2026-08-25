afa814aebde07f54a1a3ef80845c7851887a2add
Daryl Busson <darylbusson@pop-os.localdomain>
2026-08-25 09:53:08 -0500

    Fixed in EditPatient.jsx import of api

--- frontend/src/pages/EditPatient.jsx
+++ frontend/src/pages/EditPatient.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { apiFetch } from "../api";
+import { apiFetch } from "../utils/api.js";
 
 /**
  * EditPatient Component
