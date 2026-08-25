279a19905b4f1e83f13644c32b588bc1959cb36b
Daryl Busson <darylbusson@pop-os.localdomain>
2026-08-25 10:19:05 -0500

    Fixed PatientDetails import line for api

--- frontend/src/pages/PatientDetails.jsx
+++ frontend/src/pages/PatientDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { apiFetch } from "../api"; // Centralized API helper
+import { apiFetch } from "../utils/api.js"; // Centralized API helper
 
 /**
  * PatientDetails Component
